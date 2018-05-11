2dbded19556226674b247c342b6fe7d10e8f2ee6
Razi Alavizadeh <s.r.alavizadeh@gmail.com>
2018-05-12 04:21:40 +0430

    Nicer build script

--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
+echo "Recursively clone mupdf..."
 git clone --recursive git://git.ghostscript.com/mupdf.git
 
+echo "Fix issue using Git mkdir..."
 cd mupdf
-mkdir -r generated\resources\fonts\
+mkdir generated\resources\fonts\
 
 cd platform\win32
 
+echo "Upgrade all project files..."
 devenv /upgrade mupdf.sln
 
+echo "Start building..."
 msbuild /m mupdf.sln /property:Configuration=Release
