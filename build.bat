27ed837c7bc6d95a3db41fdc98a8e5e130f5a47d
Razi Alavizadeh <s.r.alavizadeh@gmail.com>
2018-06-12 02:39:53 +0430

    Add needed workarounds

--- build.bat
+++ build.bat
@@ -1,11 +1,16 @@
 echo "Recursively clone mupdf..."
 git clone --recursive git://git.ghostscript.com/mupdf.git
 
-echo "Fix issue using Git mkdir..."
-cd mupdf
-mkdir generated\resources\fonts\
+echo "Fix font not found errors..."
+copy resources\fonts\urw\NimbusSans-Bold.cff resources\fonts\urw\NimbusSansNarrow-Bold.cff
+copy resources\fonts\urw\NimbusSans-Oblique.cff resources\fonts\urw\NimbusSansNarrow-Oblique.cff
+copy resources\fonts\urw\NimbusSans-Regular.cff resources\fonts\urw\NimbusSansNarrow-Regular.cff
+copy resources\fonts\urw\NimbusSans-BoldOblique.cff resources\fonts\urw\NimbusSansNarrow-BdOblique.cff
 
 cd platform\win32
+
+echo "Workaround to fix compile issue..."
+mkdir x64\release\libresources
 
 IF NOT EXIST "libmupdf.vcxproj" ( echo "Upgrade all project files (maybe it takes > 10min), please wait..." && devenv /upgrade mupdf.sln ) else ( echo "Use cached vcxproj files." )
 
